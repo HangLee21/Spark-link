8973e502e87e4734000ff5545f82a1fc61bdbc39
lh <1411186296@qq.com>
2024-01-19 06:37:44 +0800

    lh

--- show_img.py
+++ show_img.py
@@ -1,14 +1,14 @@
 import cv2
 
-image_path='./img/'
+image_path='./demarcate/'
 
 for i in range(0,4):
-    path=image_path+str(i)+'_0.png'
+    path=image_path+'undistorted_image_'+str(i)+'.png'
     print(path)
     img=cv2.imread(path)
 
 
-    cv2.imshow('0',img)
+    cv2.imshow(str(i)+'0',img)
     key=cv2.waitKey(0)
     
     if key==27:
--- splice/splicer/splicer.h
+++ splice/splicer/splicer.h
@@ -5,10 +5,18 @@
 #pragma once
 
 #include "av_processor.h"
-#include "srcs/common.h"
+
 extern "C" {
 #include "libswscale/swscale.h"
+#include<GL/gl.h>
 }
+#include <iostream>
+#include <map>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 namespace gtoe_emulation {
 
@@ -41,3 +49,107 @@ private:
 };
 
 } // namespace gtoe_emulation
+
+static const  char* camera_names[4] = {
+    "front", "left", "back", "right"
+};
+
+static const  char* camera_flip_mir[4] = {
+    "n", "r-", "m", "r+"
+};
+//单个格子10cm
+//--------------------------------------------------------------------
+//(shift_width, shift_height): how far away the birdview looks outside
+//of the calibration pattern in horizontal and vertical directions
+static const  int shift_w = 230;
+static const  int shift_h = 230;
+
+static const  int cali_map_w  = 800;
+static const  int cali_map_h  = 800;
+//size of the gap between the calibration pattern and the car
+//in horizontal and vertical directions
+static const  int inn_shift_w = 400;
+static const  int inn_shift_h = 400;
+
+//total width/height of the stitched image
+static const  int total_w = cali_map_w + 2 * shift_w;
+static const  int total_h = cali_map_h + 2 * shift_h;
+
+//four corners of the rectangular region occupied by the car
+//top-left (x_left, y_top), bottom-right (x_right, y_bottom)
+static const  int xl = shift_w +  inn_shift_w;
+static const  int xr = total_w - xl;
+static const  int yt = shift_h + inn_shift_h;
+static const  int yb = total_h - yt;
+//--------------------------------------------------------------------
+
+static std::map<std::string, cv::Size> project_shapes = {
+    {"front",  cv::Size(total_w, yt)},
+    {"back",   cv::Size(total_w, yt)},
+    {"left",   cv::Size(total_h, xl)},
+    {"right",  cv::Size(total_h, xl)},
+};
+
+//pixel locations of the four points to be chosen.
+//you must click these pixels in the same order when running
+//the get_projection_map.py script
+static std::map<std::string, std::vector<cv::Point2f>> project_keypoints = {
+    {"front", {cv::Point2f(shift_w + 120, shift_h),
+              cv::Point2f(shift_w + 480, shift_h),
+              cv::Point2f(shift_w + 120, shift_h + 160),
+              cv::Point2f(shift_w + 480, shift_h + 160)}},
+
+    {"back", {cv::Point2f(shift_w + 120, shift_h),
+              cv::Point2f(shift_w + 480, shift_h),
+              cv::Point2f(shift_w + 120, shift_h + 160),
+              cv::Point2f(shift_w + 480, shift_h + 160)}},
+
+    {"left", {cv::Point2f(shift_h + 280, shift_w),
+              cv::Point2f(shift_h + 840, shift_w),
+              cv::Point2f(shift_h + 280, shift_w + 160),
+              cv::Point2f(shift_h + 840, shift_w + 160)}},
+
+    {"right", {cv::Point2f(shift_h + 160, shift_w),
+              cv::Point2f(shift_h + 720, shift_w),
+              cv::Point2f(shift_h + 160, shift_w + 160),
+              cv::Point2f(shift_h + 720, shift_w + 160)}}
+};
+
+struct CameraPrms
+{
+    std::string name;
+    cv::Mat dist_coff;
+    cv::Mat camera_matrix;
+    cv::Mat project_matrix;
+    cv::Mat trans_matrix;
+    cv::Size size;
+
+    cv::Mat scale_xy;
+    cv::Mat shift_xy;
+};
+
+struct BgrSts {
+    int b;
+    int g;
+    int r;
+
+    BgrSts() {
+        b = g = r = 0;
+    }
+};
+
+template<typename _T>
+static inline _T clip(float data, int max)
+{
+    if (data > max)
+        return max;
+    return (_T)data;
+}
+
+void display_mat(cv::Mat& img, std::string name);
+bool read_prms(const std::string& path, CameraPrms& prms);
+bool save_prms(const std::string& path, CameraPrms& prms);
+void undist_by_remap(const cv::Mat& src, cv::Mat& dst, const CameraPrms& prms);
+
+void merge_image(const cv::Mat& src1, const cv::Mat& src2, const cv::Mat& w, const cv::Mat& out);
+void awb_and_lum_banlance(std::vector<cv::Mat*> srcs);
